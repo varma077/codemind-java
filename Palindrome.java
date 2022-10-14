978da22a5bbd3c956c71954a7caddd914a5ff036
varma077 <97425766+varma077@users.noreply.github.com>
2022-10-14 20:29:44 +0530

    import java.util.Scanner; class Palindrome {     public static void main(String args[])     {         Scanner scan=new Scanner(System.in);         int n=scan.nextInt();         int rev=0,temp=n;         while(temp>0)         {             rev=rev*10+(temp%10);             temp/=10;         }         if(n==rev)         System.out.println("True");         else         System.out.println("False");     } }

---
+++ Palindrome.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+class Palindrome
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        int n=scan.nextInt();
+        int rev=0,temp=n;
+        while(temp>0)
+        {
+            rev=rev*10+(temp%10);
+            temp/=10;
+        }
+        if(n==rev)
+        System.out.println("True");
+        else
+        System.out.println("False");
+    }
+}
